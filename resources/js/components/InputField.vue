db48f807a50a40b332983719e0d4f0ede3a7287a
NathanielNat <psalmnat@gmail.com>
2020-10-14 09:32:18 +0000

    error handlig on form

--- resources/js/components/InputField.vue
+++ resources/js/components/InputField.vue
@@ -1,7 +1,8 @@
 <template>
         <div class="relative pt-8">
               <label :for="name" class="text-teal-500 text-uppercase font-bold text-small absolute ">{{label}}</label>
-              <input :id="name" type="text" class="pt-8 w-full border-b pb-2 focus:outline-none focus:border-teal-400 text-gray-900" :placeholder="placeholder" v-model="value" @input="updateField()">
+              <input :id="name" type="text" class="pt-8 w-full border-b pb-2 focus:outline-none focus:border-teal-400 text-gray-900" :class="erorClassObject()" :placeholder="placeholder" v-model="value" @input="updateField()">
+              <p class="text-red-600 text-sm" v-text="errorMessage()">Hi</p>
           </div>
    
 </template> 
@@ -11,7 +12,7 @@
         name: 'InputField',
 
         props: [
-            'name', 'label', 'placeholder', 'data'
+            'name', 'label', 'placeholder', 'errors'
         ],
 
         data: function() {
@@ -21,13 +22,32 @@
         },
         methods: {
             updateField: function(){
+                this.clearErrors(this.name)
+
                this.$emit('update:field',this.value)  
+            },
+            errorMessage: function () {
+                if(this.errors && this.errors[this.name] && this.errors[this.name].length >0){
+                    return  this.errors[this.name][0]
+                }
+            },
+            clearErrors: function(){
+                 if(this.errors && this.errors[this.name] && this.errors[this.name].length >0){
+                    this.errors[this.name] = null
+                }
+            },
+            erorClassObject: function(){
+             return {
+                  'error-field': this.errors && this.errors[this.name] && this.errors[this.name].length >0
+             }
             }
         }
     }
 
 </script>
 
-<style>
-
+<style scoped>
+.error-field {
+        @apply .border-red-500 .border-b-2
+    }
 </style>
--- resources/js/views/WishesCreate.vue
+++ resources/js/views/WishesCreate.vue
@@ -1,9 +1,9 @@
 <template>
     <div>   
       <form @submit.prevent="submitForm">
-          <InputField name="name" label="Name of your wish" placeholder="Life" @update:field="form.name=$event"/>
+          <InputField name="name" label="Name of your wish" placeholder="Life" :errors="errors" @update:field="form.name=$event"/>
 
-          <InputField name="wish" label="What do you wish for?" placeholder="" @update:field="form.wish=$event"/>
+          <InputField name="wish" label="What do you wish for?" placeholder="" :errors="errors" @update:field="form.wish=$event"/>
              <div class="flex justify-end ">
                 <button class="py-2 px-4 rounded text-red-700 border mr-5 hover:border-red-700 mt-4">Cancel</button>
                 <button class="bg-teal-500 py-2 px-4 text-white rounded hover:bg-teal-400 mt-4">Make Wish</button>
@@ -17,7 +17,7 @@
 import InputField from '../components/InputField';
 
 export default {
-     
+      
     name: 'WishesCreate',
 
     components:{
@@ -29,7 +29,8 @@ export default {
             form:{
                 'name':'', 
                 'wish':''
-                }
+                },
+            errors:null
         }
     },
     methods: {
@@ -37,7 +38,7 @@ export default {
             axios.post('/api/wishes',this.form).then(response =>{
 
             }).catch(errors=>{
-                
+                this.errors = errors.response.data.errors
             })
         }
     }   
